7b4031ae8a364664c495e9630be0f0896e621568
zxzx <oscarmiguelayalaruelas@gmail.com>
2021-05-06 02:27:23 -0700

    Mensajes de error en el registro agregados

--- src/app/auth/pages/register/register.page.ts
+++ src/app/auth/pages/register/register.page.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MenuController, AlertController } from '@ionic/angular';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { ValidatorService } from '../../services/validator.service';
 
 @Component({
   selector: 'app-register',
@@ -11,28 +12,104 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage implements OnInit {
 
-  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  // Patrones
+  namePattern: string = '[a-zA-Z ]{3,50}';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   constructor( private formBuilder: FormBuilder,
                private menu: MenuController,
                private alertCtrl: AlertController,
                private router: Router,
-               private authService: AuthService, ) {
+               private authService: AuthService,
+               private validatorService: ValidatorService ) {
                 this.menu.enable( false, 'first' );
               }
 
   form: FormGroup = this.formBuilder.group({
-    name: [ '', [ Validators.required ] ],
-    lastName: [ '', [ Validators.required ] ],
+    name: [ '', [ Validators.required, Validators.minLength( 3 ), Validators.pattern( this.namePattern) ] ],
+    lastName: [ '', [ Validators.required, Validators.minLength( 3 ), Validators.pattern( this.namePattern ) ] ],
     email: [ '', [ Validators.required, Validators.pattern( this.emailPattern ) ], ],
-    phone: [ '', [ Validators.required ] ],
-    password: [ '', [ Validators.required ] ],
-    password2: [ '', [ Validators.required ] ]
-  })
+    phone: [ '', [ Validators.required, Validators.minLength( 8 ) ] ],
+    password: [ '', [ Validators.required, Validators.minLength( 6 ), Validators.maxLength( 20 ) ] ],
+    password2: [ '', ]
+  },{
+    validators: [ this.validatorService.camposIguales( 'password', 'password2' ) ]
+  });
 
+  // Mensajes de error del nombre.
+  get nameErrorMsg(): string {
+    const errors = this.form.get( 'name' )?.errors;
+    if( errors?.required ) {
+      return 'El nombre es obligatorio.';
+    } else if ( errors?.minlength ) {
+      return 'El nombre debe contener mínimo 3 caracteres.';
+    } else if ( errors?.pattern ) {
+      return 'El nombre no es válido.';
+    }
+    return '';
+  }
+
+  // Mensajes de error del apellido.
+  get lastNameErrorMsg(): string {
+    const errors = this.form.get( 'lastName' )?.errors;
+    if( errors?.required ) {
+      return 'El apellido es obligatorio.';
+    } else if( errors?.minlength ) {
+      return 'El apellido debe contener mínimo 3 caracteres.';
+    } else if( errors?.pattern ) {
+      return 'El apellido no es válido';
+    }
+    return '';
+  }
+
+  // Mensajes de error del correo
+  get emailErrorMsg(): string {
+    const errors = this.form.get( 'email' )?.errors;
+    if( errors?.required ) {
+      return 'El correo es obligatorio.';
+    } else if( errors?.pattern ) {
+      return 'El formato del correo no es válido.'
+    }
+    return '';
+  }
+
+  // Mensajes de error del teléfono
+  get phoneErrorMsg(): string {
+    const errors = this.form.get( 'phone' )?.errors;
+    if ( errors?.required ) {
+      return 'El teléfono es obligatorio';
+    } else if ( errors?.minlength ) {
+      return 'El teléfono debe contener mínimio 8 caracteres.';
+    } // TODO: Patrón.
+    return '';
+  }
+
+  // Mensajes de error de la contraseña
+  get passwordErrorMsg(): string {
+    const errors = this.form.get( 'password' )?.errors;
+    if ( errors?.required ) {
+      return 'La contraseña es obligatoria.';
+    } else if ( errors?.minlength ) {
+      return 'La contraseña debe contener mínimo 6 caracteres.';
+    } else if( errors?.maxlength ) {
+      return 'La contraseña debe contener máximo 20 caracteres.';
+    }
+    return '';
+  }
+
+  // Mensaje de error para confirmar la contraseña
+  get confirmPasswordErrorMsg(): string {
+    const errors = this.form.get( 'password2' )?.errors;
+    if ( errors?.noIguales !== false ) {
+      return 'Las contraseñas no son iguales.';
+    }
+    console.log( errors );
+    return '';
+  }
   ngOnInit() {
   }
 
+  // Registrar usuario.
   async onRegister() {
 
     const { email, password } = this.form.value;
@@ -42,6 +119,10 @@ export class RegisterPage implements OnInit {
     delete userForm.password2;
 
     try {
+      if ( !this.form.valid ) {
+        this.form.markAllAsTouched();
+        return;
+      }
       const user = await this.authService.registerWithEmailPassword( email, password, userForm );
 
       if ( user ) {
@@ -55,7 +136,7 @@ export class RegisterPage implements OnInit {
           role: 'Empleado',
           uid: user.uid
         }
-        this.authService.addUser( userData );
+        // this.authService.addUser( userData );
       }
     } catch ( err ) {
       console.log("🚀 ~ file: register.page.ts ~ line 40 ~ RegisterPage ~ onRegister ~ err", err);
@@ -63,22 +144,27 @@ export class RegisterPage implements OnInit {
 
   }
 
-    // Verify Email Alert
-    async verifyEmailAlert() {
-      const alert = await this.alertCtrl.create({
-        header: 'Registrado correctamente',
-        message: 'Verifique su correo',
-        buttons: [
-          {
-            text: 'OK',
-            handler: () => {
-              this.router.navigate( ['/login'] );
-            }
-          }
-        ]
-      });
-  
-      await alert.present();
-    }
+  // Mensajes de errores
+  campoNoValido( campo: string ) {
+    return this.form.get( campo )?.invalid
+          && this.form.get( campo )?.touched;
+  }
 
+  // Alerta de verificar correo.
+  async verifyEmailAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Registrado correctamente',
+      message: 'Verifique su correo',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.router.navigate( ['/login'] );
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
 }
